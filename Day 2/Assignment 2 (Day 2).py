56dcd0981bbf3f8f252b9b5b71838b9337a310b9
ROGU3D3viL <ROGU3D3viL@users.noreply.github.com>
2025-09-26 20:20:17 +0530

    Good

--- Day 2/Assignment 2 (Day 2).py
+++ Day 2/Assignment 2 (Day 2).py
@@ -1,15 +1,22 @@
 print("==================== Certificate Eligibility Checker ==================")
-days = int(input("Enter number of days you attended the training: "))
+days = int(input("Enter number of days you attended the training out of 5: "))
 if days == 5:
-    for i in range(1, 5):
+    for i in range(1, 5 + 1):
         print(f"Day {i} Feedback:")
         asgn = input("Was Assignment Done? (Yes/No): ")
         attd = input("Was Attendance Given? (Yes/No): ")
         cam = input("Was Camera On? (Yes/No): ")
         if asgn == "Yes" and attd == "Yes" and cam == "Yes":
-            continue
+            if i == 5:
+                print("You are eligible for the certificate...!")
+                break
+            else:
+                continue
+            
         else:
             print("You are not eligible for the certificate...!")
             break
-else:
+elif days < 5:
     print("You are not eligible for the certificate...!")
+else:
+    print("Wrong Input Given...!")
