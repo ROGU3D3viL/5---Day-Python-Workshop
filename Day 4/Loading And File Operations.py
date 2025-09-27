7d0ecf25096f48ac1aaf2f177f2145191453df77
ROGU3D3viL <ROGU3D3viL@users.noreply.github.com>
2025-09-27 20:13:50 +0530

    Good

--- Day 4/Loading And File Operations.py
+++ Day 4/Loading And File Operations.py
@@ -1,8 +1,8 @@
 import pandas as pd
 df = pd.read_csv("C:/Users/User/OneDrive/Documents/VS Code Documents/Python Workshop For Data Analytics/Study Materials/Projects/Modified_Indian_Employee_Dataset.csv")
-print(df)
-print(df.head(5))
-print(df.tail(5))
+print(df) # Print the dataframe
+print(df.head(5)) # First 5 rows of the dataframe
+print(df.tail(5)) # Last 5 rows of the dataframe
 print(df.shape) # (rows, columns) of the dataframe
 print(df.columns) # Column names of the dataframe
 print(df.info()) # Concise summary of the dataframe
